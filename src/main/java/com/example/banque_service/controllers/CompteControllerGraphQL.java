3b2a3eaebce5b09bdb9a29d1e1827d3ad53c3c18
Rzn-Mohamed <meddrazin12@gmail.com>
2025-11-17 19:02:45 +0100

    tp15

--- src/main/java/com/example/banque_service/controllers/CompteControllerGraphQL.java
+++ src/main/java/com/example/banque_service/controllers/CompteControllerGraphQL.java
@@ -1,10 +1,15 @@
 package com.example.banque_service.controllers;
 
+
+import com.example.banque_service.entities.Compte;
+import com.example.banque_service.repositories.CompteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
--- src/main/java/com/example/banque_service/repositories/CompteRepository.java
+++ src/main/java/com/example/banque_service/repositories/CompteRepository.java
@@ -1,4 +1,17 @@
 package com.example.banque_service.repositories;
 
-public class CompteRepository {
+import com.example.banque_service.entities.Compte;
+import com.example.banque_service.entities.TypeCompte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+    List<Compte> findByType(TypeCompte type);
+    
+    List<Compte> findBySoldeGreaterThan(double solde);
+
+    double sumSoldes();
 }
